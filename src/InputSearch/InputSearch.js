1bd76c8b08fff0612d3299fe328bd924eb6569f8
Sanjay Shahu <sanjay.shahu@nagarro.com>
2024-06-19 19:18:55 +0530

    icon-added

--- src/InputSearch/InputSearch.js
+++ src/InputSearch/InputSearch.js
@@ -9,12 +9,37 @@ function InputSearch({searchQuery,setSearchQuery}) {
 
 
     return (
+        <div style={{ position: 'relative', width: '100%', marginBottom: '20px' }}>
         <input
             type="text"
             placeholder="Search..."
             value={searchQuery}
             onChange={handleSearch}
-            style={{ marginBottom: "20px", padding: "10px", width: "100%" }}
-        />)
+            style={{ padding: '10px', 
+                  width: '100%',
+                 paddingRight: '40px',
+                 backgroundImage: 'url(/images/nav_bar.png)', // Use the imported image
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                border: 'none', 
+                outline: 'none',
+                color: 'white', 
+                fontSize: '16px' 
+
+             }} 
+        />
+        <img
+            src="/images/search.png"
+            alt="search icon"
+            style={{
+                position: 'absolute',
+                right: '10px',
+                top: '25%',
+                height: '20px', 
+                pointerEvents: 'none' 
+            }}
+        />
+    </div>
+    )
 }
 export default InputSearch;
